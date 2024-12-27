19ef7ac387349c244f88e35ec157da039e32b75b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-12-27 20:03:36 +0300

    feat : color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,30 @@ module.exports = {
   theme: {
     extend: {
       fontFamily : {
-        rubik : ['Rubik-Regular', 'Rubik-Medium', 'Rubik-SemiBold', 'Rubik-Bold', 'Rubik-ExtraBold', 'Rubik-Light'],
-      }
+        rubik : ['Rubik-Regular', 'sans-serif'],
+        rubikBold : ['Rubik-Bold', 'sans-serif'],
+        rubikExtraBold : ['Rubik-ExtraBold', 'sans-serif'],
+        rubikSemiBold : ['Rubik-SemiBold', 'sans-serif'],
+        rubikMedium : ['Rubik-Medium', 'sans-serif'],
+        rubikLight : ['Rubik-Light', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          100: "#0061FF0A",
+          200: "#0061FF1A",
+          300: "#0061FF",
+        },
+        accent: {
+          100: "#FBFBFD",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
+      },
     },
   },
   plugins: [],
