3bd6306c403bfc0379da989cea22b28f6a0cbbca
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-12-27 20:00:29 +0300

    feat : load font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,25 @@
 import "./global.css"
-import { Stack } from "expo-router";
-
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-light": require("../assets/fonts/Rubik-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded) return null;
+  
   return <Stack />;
 }
