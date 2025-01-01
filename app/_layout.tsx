e783a8005c085d6caec580c92cf238b95d0884f7
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-01-01 21:08:07 +0300

    feat : global context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import "./global.css"
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/globalProvider";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -21,5 +22,9 @@ export default function RootLayout() {
 
   if(!fontsLoaded) return null;
   
-  return <Stack screenOptions={{ headerShown : false }} />;
+  return(
+    <GlobalProvider>
+       <Stack screenOptions={{ headerShown : false }} />
+    </GlobalProvider>
+  )
 }
