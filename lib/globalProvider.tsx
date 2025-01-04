ecf4a385143a6cacbdd32336be43e0560714a2f4
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-01-04 20:05:49 +0300

    feat : use real data on profile page

--- lib/globalProvider.tsx
+++ lib/globalProvider.tsx
@@ -29,8 +29,6 @@ const GlobalProvider = ({ children }: { children: ReactNode }) => {
 
     const isLoggedin = !!user;
 
-    console.log(JSON.stringify(user,null,2))
-
     return (
         <GlobalContext.Provider value={{
             isLoggedin,
